dc5fd5e0e7613300b77e796e53c6212d748d28d2
Gunnar Ingi Stefánsson <gstefansson@esri.com>
2024-08-28 09:22:01 +0200

    removes unused `ClientOnly` component

--- app/components/client-only.tsx
+++
@@ -1,8 +0,0 @@
-import { PropsWithChildren, useSyncExternalStore } from "react";
-
-export default function ClientOnly({ children }: PropsWithChildren) {
-  const env = useSyncExternalStore(() => () => { }, () => 'client', () => 'server');
-
-  if (env === 'server') return null;
-  return children;
-}
